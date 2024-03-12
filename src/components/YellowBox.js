4215985e866fa70c69f702e08d8ff2c84280348b
xininny <xininny@gmail.com>
2024-03-12 17:46:40 +0900

    Feat. third test

--- src/components/YellowBox.js
+++ src/components/YellowBox.js
@@ -5,7 +5,7 @@ import '../css/YellowBox.css';
 const YellowBox = ({ username, itemCount }) => {
     return (
         <div className="yellow-box">
-            <p className="greeting">환영합니다, {username}님</p>
+            <p className="greeting">환영합니다! {username}님</p>
             <p className="prompt">오늘은 어떤 물건을 공유할까요?</p>
             <p className="item-count">
                 지금 내가 빌려준 물건<span className="count">{itemCount}</span>
