e886809b3a2bbac5c68e8ac2cbd39ca0c52a4f43
xininny <xininny@gmail.com>
2024-03-12 19:49:51 +0900

    Feat. ItemRow & ItemPreview

--- src/pages/Mainpage.js
+++ src/pages/Mainpage.js
@@ -1,27 +1,33 @@
-import React from "react";
-import "./Mainpage.css";
-import Topnav from "../components/Topnav";
-import YellowBox from "../components/YellowBox";
-import GreenBox from "../components/GreenBox";
-import Calendar from "../components/Calendar";
-import PurpleBox from "../components/PurpleBox";
-import Footer from "../components/Footer";
+import React from 'react';
+import './Mainpage.css';
+import Topnav from '../components/Topnav';
+import YellowBox from '../components/YellowBox';
+import GreenBox from '../components/GreenBox';
+import Calendar from '../components/Calendar';
+import PurpleBox from '../components/PurpleBox';
+import Footer from '../components/Footer';
+import ItemRow from '../components/ItemRow';
 
 function Mainpage() {
-  return (
-    <div className="container">
-      <Topnav></Topnav>
-      <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
-        <YellowBox />
-        <Calendar />
-      </div>
-      <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
-        <GreenBox style={{ flex: 1 }} />
-        <PurpleBox style={{ flex: 1 }} />
-      </div>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="container">
+            <Topnav></Topnav>
+            <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+                <YellowBox />
+                <Calendar />
+            </div>
+            <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+                <GreenBox style={{ flex: 1 }} />
+                <PurpleBox style={{ flex: 1 }} />
+            </div>
+            <div>주변엔 이런 물건을 빌릴 수 있어요</div>
+            <div>
+                <ItemRow></ItemRow>
+            </div>
+
+            <Footer />
+        </div>
+    );
 }
 
 export default Mainpage;
