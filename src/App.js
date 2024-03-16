a4951c3ac280c56621c237caf91862cbdb1e3da9
xininny <xininny@gmail.com>
2024-03-17 00:14:24 +0900

    Feat. SideMenu

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Mainpage from './pages/Mainpage';
+import LendMain from './pages/LendMain';
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" Component={Mainpage} />
+                <Route path="/" element={<Mainpage />} />
+                <Route path="/lend" element={<LendMain />} />
                 {/* Add more routes here for other pages */}
             </Routes>
         </BrowserRouter>
--- src/components/PublicSideMenu.js
+++ src/components/PublicSideMenu.js
@@ -1,13 +1,17 @@
-import React from "react";
-import "../css/PublicSideMenu.css";
+//PubicSideMenu.js
 
-function PublicSideMenu({ Menu_Text, Menu_Image }) {
-  return (
-    <div className="PublicSideMenu">
-      <img src={process.env.PUBLIC_URL + Menu_Image} alt="MenuImage" />
-      <div>{Menu_Text}</div>
-    </div>
-  );
+import React from 'react';
+import '../css/PublicSideMenu.css';
+
+function PublicSideMenu({ Menu_Text, Menu_Link, Menu_Image, isActive, onClick }) {
+    return (
+        <div className={`PublicSideMenu ${isActive ? 'active' : ''}`}>
+            <a href={Menu_Link} className="menu-link" onClick={onClick}>
+                <img src={process.env.PUBLIC_URL + Menu_Image} alt="MenuImage" className="menu-image" />
+                <span className="menu-text">{Menu_Text}</span>
+            </a>
+        </div>
+    );
 }
 
 export default PublicSideMenu;
