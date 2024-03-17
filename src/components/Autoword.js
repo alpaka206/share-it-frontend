a461cacec25bf04fbd21e36f2fc1b3ba02d148d8
alpaka206 <gyuwon05@gmail.com>
2024-03-17 15:06:56 +0900

    feat. add Search.svg and add nav componenets

---
+++ src/components/Autoword.js
@@ -0,0 +1,43 @@
+// Autoword.js
+import React, { useState, useEffect } from "react";
+
+const Autoword = ({ keyword }) => {
+  const [keyItems, setKeyItems] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(
+          `https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json`
+        );
+        const data = await res.json();
+        const filteredData = data
+          .filter((list) => list.city.includes(keyword))
+          .slice(0, 10);
+        setKeyItems(filteredData);
+      } catch (error) {
+        console.error("에러 발생:", error);
+      }
+    };
+
+    if (keyword.trim() !== "") {
+      fetchData();
+    } else {
+      setKeyItems([]); // 검색어가 비어있을 경우 목록 초기화
+    }
+  }, [keyword]);
+
+  return (
+    <div className="auto-complete" style={{ width: "230px" }}>
+      {keyItems.length > 0 && (
+        <div className="auto-complete">
+          {keyItems.map((item) => (
+            <div key={item.city}>{item.city}</div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Autoword;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,103 +1,107 @@
 //Sidebar.js
 
-import React, { useState } from 'react';
-import '../css/Sidebar.css';
-import PublicSideMenu from './PublicSideMenu';
-import { useLocation, useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import "../css/Sidebar.css";
+import PublicSideMenu from "./PublicSideMenu";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function Sidebar({ isOpen, onClose }) {
-    const navigate = useNavigate();
-    const location = useLocation();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    const [activeMenuLink, setActiveMenuLink] = useState('/');
+  const [activeMenuLink, setActiveMenuLink] = useState("/");
 
-    React.useEffect(() => {
-        setActiveMenuLink(location.pathname);
-    }, [location.pathname]);
+  React.useEffect(() => {
+    setActiveMenuLink(location.pathname);
+  }, [location.pathname]);
 
-    const handleMenuClick = (menuLink) => {
-        onClose();
-        setActiveMenuLink(menuLink);
-        navigate(menuLink);
-    };
+  const handleMenuClick = (menuLink) => {
+    onClose();
+    setActiveMenuLink(menuLink);
+    navigate(menuLink);
+  };
 
-    return (
-        <div className={`sidebar ${isOpen ? 'open' : ''}`}>
-            <div className="sidebar-header">
-                <img src={process.env.PUBLIC_URL + `assets/logo.svg`} alt="logo" className="logo" />{' '}
-                <button className="close-btn" onClick={onClose}>
-                    <img src={process.env.PUBLIC_URL + '/assets/x.svg'} alt="Close" />
-                </button>
-            </div>
-            <div className="home-group">
-                <PublicSideMenu
-                    Menu_Text="홈"
-                    Menu_Link="/"
-                    Menu_Image={`assets/home.svg`}
-                    isActive={activeMenuLink === '/'}
-                    onClick={() => handleMenuClick('/')}
-                />
-                <PublicSideMenu
-                    Menu_Text="통합 검색"
-                    Menu_Link="/search"
-                    Menu_Image={`assets/search.png`}
-                    isActive={activeMenuLink === '/search'}
-                    onClick={() => handleMenuClick('/search')}
-                />
-                <PublicSideMenu
-                    Menu_Text="채팅"
-                    Menu_Link="/chat"
-                    Menu_Image={`assets/chat.svg`}
-                    isActive={activeMenuLink === '/chat'}
-                    onClick={() => handleMenuClick('/chat')}
-                />
-            </div>
-            <div className="space"></div>
-            <div className="lend-group">
-                <PublicSideMenu
-                    Menu_Text="빌려주기"
-                    Menu_Link="/lend"
-                    Menu_Image={`assets/borrow.svg`}
-                    isActive={activeMenuLink === '/lend'}
-                    onClick={() => handleMenuClick('/lend')}
-                />
-                <PublicSideMenu
-                    Menu_Text="필요해요"
-                    Menu_Link="/need"
-                    Menu_Image={`assets/lend.svg`}
-                    isActive={activeMenuLink === '/need'}
-                    onClick={() => handleMenuClick('/need')}
-                />
-            </div>
-            <div className="space"></div>
-            <div className="evaluate-group">
-                <PublicSideMenu
-                    Menu_Text="평가하기"
-                    Menu_Link="/evaluate"
-                    Menu_Image={`assets/evaluate.svg`}
-                    isActive={activeMenuLink === '/evaluate'}
-                    onClick={() => handleMenuClick('/evaluate')}
-                />
-                <PublicSideMenu
-                    Menu_Text="마이페이지"
-                    Menu_Link="/mypage"
-                    Menu_Image={`assets/mypage.svg`}
-                    isActive={activeMenuLink === '/mypage'}
-                    onClick={() => handleMenuClick('/mypage')}
-                />
-            </div>
-            <div className="logout-space"></div>
-            <div className="logout-group">
-                <PublicSideMenu
-                    Menu_Text="로그아웃"
-                    Menu_Link="/logout"
-                    Menu_Image={`assets/logout.svg`}
-                    isActive={activeMenuLink === '/logout'}
-                    onClick={() => handleMenuClick('/logout')}
-                />
-            </div>
-        </div>
-    );
+  return (
+    <div className={`sidebar ${isOpen ? "open" : ""}`}>
+      <div className="sidebar-header">
+        <img
+          src={process.env.PUBLIC_URL + `assets/logo.svg`}
+          alt="logo"
+          className="logo"
+        />{" "}
+        <button className="close-btn" onClick={onClose}>
+          <img src={process.env.PUBLIC_URL + "/assets/x.svg"} alt="Close" />
+        </button>
+      </div>
+      <div className="home-group">
+        <PublicSideMenu
+          Menu_Text="홈"
+          Menu_Link="/"
+          Menu_Image={`assets/home.svg`}
+          isActive={activeMenuLink === "/"}
+          onClick={() => handleMenuClick("/")}
+        />
+        <PublicSideMenu
+          Menu_Text="통합 검색"
+          Menu_Link="/search"
+          Menu_Image={`assets/Search.svg`}
+          isActive={activeMenuLink === "/search"}
+          onClick={() => handleMenuClick("/search")}
+        />
+        <PublicSideMenu
+          Menu_Text="채팅"
+          Menu_Link="/chat"
+          Menu_Image={`assets/chat.svg`}
+          isActive={activeMenuLink === "/chat"}
+          onClick={() => handleMenuClick("/chat")}
+        />
+      </div>
+      <div className="space"></div>
+      <div className="lend-group">
+        <PublicSideMenu
+          Menu_Text="빌려주기"
+          Menu_Link="/lend"
+          Menu_Image={`assets/borrow.svg`}
+          isActive={activeMenuLink === "/lend"}
+          onClick={() => handleMenuClick("/lend")}
+        />
+        <PublicSideMenu
+          Menu_Text="필요해요"
+          Menu_Link="/need"
+          Menu_Image={`assets/lend.svg`}
+          isActive={activeMenuLink === "/need"}
+          onClick={() => handleMenuClick("/need")}
+        />
+      </div>
+      <div className="space"></div>
+      <div className="evaluate-group">
+        <PublicSideMenu
+          Menu_Text="평가하기"
+          Menu_Link="/evaluate"
+          Menu_Image={`assets/evaluate.svg`}
+          isActive={activeMenuLink === "/evaluate"}
+          onClick={() => handleMenuClick("/evaluate")}
+        />
+        <PublicSideMenu
+          Menu_Text="마이페이지"
+          Menu_Link="/mypage"
+          Menu_Image={`assets/mypage.svg`}
+          isActive={activeMenuLink === "/mypage"}
+          onClick={() => handleMenuClick("/mypage")}
+        />
+      </div>
+      <div className="logout-space"></div>
+      <div className="logout-group">
+        <PublicSideMenu
+          Menu_Text="로그아웃"
+          Menu_Link="/logout"
+          Menu_Image={`assets/logout.svg`}
+          isActive={activeMenuLink === "/logout"}
+          onClick={() => handleMenuClick("/logout")}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
