c940ed43b18166bc63d46724b93f4dbec42b93db
alpaka206 <gyuwon05@gmail.com>
2024-03-17 16:29:21 +0900

    feat. add Autoword

--- src/components/Topnav.js
+++ src/components/Topnav.js
@@ -3,12 +3,20 @@ import Sidebar from "./Sidebar";
 import "../css/Topnav.css";
 import PublicButton from "./PublicButton";
 import SearchContainer from "./SearchContainer";
+
 function Topnav() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // 로그인 상태를 나타내는 상태값 추가
 
   const toggleMenu = () => {
     setIsMenuOpen((prevState) => !prevState);
   };
+
+  // useEffect(() => {
+
+  //   const userIsLoggedIn = true;
+  //   setIsLoggedIn(userIsLoggedIn);
+  // }, []);
 
   return (
     <div className="header">
@@ -31,33 +39,29 @@ function Topnav() {
         <Sidebar isOpen={isMenuOpen} onClose={toggleMenu} />
         <div className="right-header">
           <SearchContainer />
-          <PublicButton
-            Button_Text="알림"
-            Button_Image={`assets/alert.svg`}
-          ></PublicButton>
-          <PublicButton
-            Button_Text="마이페이지"
-            Button_Image={`assets/mypage.svg`}
-          ></PublicButton>
+
+          <PublicButton Button_Text="알림" Button_Image={`assets/alert.svg`} />
+          {isLoggedIn ? (
+            <PublicButton
+              Button_Text="마이페이지"
+              Button_Image={`assets/mypage.svg`}
+            />
+          ) : (
+            <PublicButton
+              Button_Text="로그인"
+              Button_Image={`assets/mypage.svg`}
+            />
+          )}
         </div>
       </div>
       <div className="nav_bottom">
-        <PublicButton
-          Button_Text="홈"
-          Button_Image={`assets/home.svg`}
-        ></PublicButton>
+        <PublicButton Button_Text="홈" Button_Image={`assets/home.svg`} />
         <PublicButton
           Button_Text="통합검색"
           Button_Image={`assets/Search.svg`}
-        ></PublicButton>
-        <PublicButton
-          Button_Text="빌리기"
-          Button_Image={`assets/borrow.svg`}
-        ></PublicButton>
-        <PublicButton
-          Button_Text="필요해요"
-          Button_Image={`assets/lend.svg`}
-        ></PublicButton>
+        />
+        <PublicButton Button_Text="빌리기" Button_Image={`assets/borrow.svg`} />
+        <PublicButton Button_Text="필요해요" Button_Image={`assets/lend.svg`} />
       </div>
     </div>
   );
