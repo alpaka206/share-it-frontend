a76e364bf8ffdffc7534ab8c55bb36c822e998f2
xininny <xininny@gmail.com>
2024-03-17 20:45:56 +0900

    Feat. css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,27 @@
 // Topnav.js
 
-import React from "react";
+import React from 'react';
+import '../css/Footer.css'; // Footer 컴포넌트의 CSS 파일을 불러옵니다.
 
 function Footer() {
-  return (
-    <div className="Footer">
-      <img
-        src={process.env.PUBLIC_URL + `assets/logo.svg`}
-        alt="logo"
-        className="logo"
-      />
-      <h3>
-        2024 가톨릭대학교 종합설계프로젝트
-        <br />
-        김규원 박승원 서승준 이건 전미진
-      </h3>
-      <h3>
-        쉐어릿은 통신판매중개자이며, 통신판매의 당사자가 아닙니다.
-        <br /> 상품, 상품 정보, 거래, 배송에 관한 의무와 책임은 판매자에게
-        있습니다.
-      </h3>{" "}
-      이용약관 | 개인정보처리방침 | Github
-      <br /> Copyright by 2024 Shareit
-    </div>
-  );
+    return (
+        <div className="Footer">
+            <img src={process.env.PUBLIC_URL + `assets/logo.svg`} alt="logo" className="logo" />
+            <p className="project-info">
+                2024 가톨릭대학교 종합설계프로젝트
+                <br />
+                김규원 박승원 서승준 이건 전미진
+            </p>
+            <p className="disclaimer">
+                쉐어릿은 통신판매중개자이며, 통신판매의 당사자가 아닙니다.
+                <br /> 상품, 상품 정보, 거래, 배송에 관한 의무와 책임은 판매자에게 있습니다.
+            </p>{' '}
+            <p className="links">
+                이용약관 | 개인정보처리방침 | Github
+                <br /> Copyright by 2024 Shareit
+            </p>
+        </div>
+    );
 }
 
 export default Footer;
--- src/pages/Mainpage.js
+++ src/pages/Mainpage.js
@@ -1,33 +1,34 @@
-import React from "react";
-import "./Mainpage.css";
-import Topnav from "../components/Topnav";
-import YellowBox from "../components/MainPage/YellowBox";
-import GreenBox from "../components/MainPage/GreenBox";
-import DotCalendar from "../components/MainPage/DotCalendar";
-import PurpleBox from "../components/MainPage/PurpleBox";
-import Footer from "../components/Footer";
-import ItemRow from "../components/MainPage/ItemRow";
+import React from 'react';
+import './Mainpage.css';
+import Topnav from '../components/Topnav';
+import YellowBox from '../components/MainPage/YellowBox';
+import GreenBox from '../components/MainPage/GreenBox';
+import DotCalendar from '../components/MainPage/DotCalendar';
+import PurpleBox from '../components/MainPage/PurpleBox';
+import Footer from '../components/Footer';
+import ItemRow from '../components/MainPage/ItemRow';
 
 function Mainpage() {
-  return (
-    <div className="container">
-      <Topnav></Topnav>
-      <div className="semi_container">
-        <YellowBox className="yellow_box" />
-        <DotCalendar className="DotCalendar" />
-      </div>
-      <div className="semi_container">
-        <GreenBox className="green_box" />
-        <PurpleBox className="purple_box" />
-      </div>
+    return (
+        <div className="container">
+            <Topnav></Topnav>
+            <div className="semi_container">
+                <YellowBox className="yellow_box" />
+                <DotCalendar className="DotCalendar" />
+            </div>
+            <div className="semi_container">
+                <GreenBox className="green_box" />
+                <PurpleBox className="purple_box" />
+            </div>
 
-      <div>주변엔 이런 물건을 빌릴 수 있어요</div>
-      <div>
-        <ItemRow></ItemRow>
-      </div>
-      <Footer />
-    </div>
-  );
+            <div className="text-wrapper">주변엔 이런 물건을 빌릴 수 있어요</div>
+            <div>
+                <ItemRow></ItemRow>
+            </div>
+
+            <Footer className="footer" />
+        </div>
+    );
 }
 
 export default Mainpage;
