a94add97bc9a6f0f40339f4d88b3a394a6801dc1
alpaka206 <gyuwon05@gmail.com>
2024-05-09 03:02:09 +0900

    feat. chat design

--- src/components/ChatingRoom.js
+++ src/components/ChatingRoom.js
@@ -1,9 +1,36 @@
 // ChatLeftPanel.js
 import React, { useState } from "react";
 import "../css/ChatingRoom.css";
+import MyChat from "./MyChat";
+import GuestChat from "./GuestChat";
 
 const ChatingRoom = () => {
-  return <div className="ChatingRoom"></div>;
+  return (
+    <div className="ChatingRoom">
+      <div className="ChatContainer">
+        <MyChat
+          myText={
+            "Longer Text and it is really longlonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglong"
+          }
+        />
+        <GuestChat
+          GuestText={
+            "Longer Text and it is really longlonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglong"
+          }
+        />
+        <MyChat
+          myText={
+            "Longer Text and it is really longlonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglong"
+          }
+        />
+        <GuestChat
+          GuestText={
+            "Longer Text and it is really longlonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglong"
+          }
+        />
+      </div>
+    </div>
+  );
 };
 
 export default ChatingRoom;
---
+++ src/components/GuestChat.js
@@ -0,0 +1,9 @@
+// ChatRightPanel.js
+import React from "react";
+import "../css/GuestChat.css";
+
+const GuestChat = ({ GuestText }) => {
+  return <div className="GuestText">{GuestText}</div>;
+};
+
+export default GuestChat;
