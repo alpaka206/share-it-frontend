f023d9efb49d091cd5d4ac16f96705ba700521ca
alpaka206 <gyuwon05@gmail.com>
2024-05-13 15:09:04 +0900

    Feat. design recentdate and add font

--- src/components/MainPage/Recentdate.js
+++ src/components/MainPage/Recentdate.js
@@ -6,13 +6,29 @@ import RecentdateElement from "./RecentdateElement";
 const Recentdate = () => {
   const navigate = useNavigate(); // useNavigate를 사용하여 navigate 함수 생성
 
+  // 데이터 배열
+  const recentItems = [
+    { Month: "MAY", date: 4, dayinfo: "반납일", itemname: "에어팟 맥스" },
+    { Month: "MAY", date: 22, dayinfo: "대여일", itemname: "로지텍 K380" },
+    { Month: "JUNE", date: 17, dayinfo: "반납일", itemname: "로지텍 K380" },
+  ];
+
   const handleButtonClick = () => {
     navigate("/trade"); // '/request' 경로로 이동
   };
 
   return (
     <div className="Recentdate">
-      <RecentdateElement />
+      {/* recentItems 배열을 매핑하여 각각의 RecentdateElement에 props로 전달 */}
+      {recentItems.map((item, index) => (
+        <RecentdateElement
+          key={index}
+          Month={item.Month}
+          date={item.date}
+          dayinfo={item.dayinfo}
+          itemname={item.itemname}
+        />
+      ))}
     </div>
   );
 };
