75c45dd8f9666cd143c070b035c03d4c69fc9440
xininny <xininny@gmail.com>
2024-05-15 03:02:30 +0900

    login css

--- src/components/Autoword.js
+++ src/components/Autoword.js
@@ -1,50 +1,41 @@
-// Autoword.js
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import wordData from "../data/wordData";
-import "../css/Autoword.css";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import wordData from '../data/wordData';
+import '../css/Autoword.css';
 
 const Autoword = ({ keyword, onSearch, className }) => {
-  // onSearch 함수 추가
-  const [keyItems, setKeyItems] = useState([]);
+    const [keyItems, setKeyItems] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      if (keyword.trim() !== "") {
-        const filteredData = wordData
-          .filter((item) => item.includes(keyword))
-          .slice(0, 10);
-        setKeyItems(filteredData);
-      } else {
-        setKeyItems([]); // 검색어가 비어있을 경우 목록 초기화
-      }
+    useEffect(() => {
+        const fetchData = async () => {
+            if (keyword.trim() !== '') {
+                const filteredData = wordData.filter((item) => item.includes(keyword)).slice(0, 10);
+                setKeyItems(filteredData);
+            } else {
+                setKeyItems([]);
+            }
+        };
+
+        fetchData();
+    }, [keyword]);
+
+    const handleWordClick = (word) => {
+        onSearch(word);
     };
 
-    fetchData();
-  }, [keyword]);
-
-  // 클릭된 단어를 검색어로 설정하고 검색 실행
-  const handleWordClick = (word) => {
-    onSearch(word);
-  };
-
-  return (
-    <div className="auto-complete-container">
-      {keyItems.length > 0 && (
-        <div className={className}>
-          {keyItems.map((item, index) => (
-            <div
-              key={index}
-              className="auto-complete-item"
-              onClick={() => handleWordClick(item)}
-            >
-              # {item}
-            </div>
-          ))}
+    return (
+        <div className="auto-complete-container">
+            {keyItems.length > 0 && (
+                <div className={className}>
+                    {keyItems.map((item, index) => (
+                        <div key={index} className="auto-complete-item" onClick={() => handleWordClick(item)}>
+                            {item}
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default Autoword;
