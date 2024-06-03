61b5937fe9ac58baeb2c11354b3a9a10942e78c8
alpaka206 <gyuwon05@gmail.com>
2024-06-03 21:26:30 +0900

    Feat. mainpage

--- src/pages/Mainpage.js
+++ src/pages/Mainpage.js
@@ -1,8 +1,38 @@
-import { useState } from 'react';
-import MainpageLogin from './MainpageLogin';
-import MainpageUnLogin from './MainpageUnLogin';
+import { useEffect, useState } from "react";
+import MainpageLogin from "./MainpageLogin";
+import MainpageUnLogin from "./MainpageUnLogin";
+import axios from "axios";
 function Mainpage() {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    return <div>{isLoggedIn ? <MainpageLogin /> : <MainpageUnLogin />}</div>;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      try {
+        const token = localStorage.getItem("token") || "";
+        const response = await axios.get(
+          "https://catholic-mibal.site/token/check",
+          {
+            headers: {
+              Authorization: token,
+            },
+          }
+        );
+        if (
+          response.data.code[0] === "SEC-001" ||
+          response.data.code[0] === "SEC-002"
+        ) {
+          setIsLoggedIn(false);
+        } else {
+          setIsLoggedIn(true);
+        }
+      } catch (error) {
+        console.error(error);
+        // setIsLoggedIn(false);
+      }
+    };
+
+    checkLoginStatus();
+  }, []);
+
+  return <div>{isLoggedIn ? <MainpageLogin /> : <MainpageUnLogin />}</div>;
 }
 export default Mainpage;
